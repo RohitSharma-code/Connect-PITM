ec3c6086d4b2dfd62ee3a3e5992e3b170cfb0930
rohit-sharma <rohitsharma792.rs@gmail.com>
2024-12-04 01:36:43 +0530

    design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
 }
 
 android {
@@ -45,4 +46,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
+
 }
--- app/src/main/java/com/example/connectpitm/MainActivity.kt
+++ app/src/main/java/com/example/connectpitm/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.connectpitm
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        slidePhoto()
+
+    }
+
+    fun slidePhoto(){
+        val imageList = ArrayList<SlideModel>() // Create image list
+
+        // imageList.add(SlideModel("String Url" or R.drawable)
+        // imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
+
+        imageList.add(SlideModel("https://i.imghippo.com/files/ihJ4255ubA.png", ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel("https://i.postimg.cc/W4Hjf4WK/counselling.jpg", ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel("https://i.postimg.cc/gkvPVWCV/picnic.jpg", ScaleTypes.CENTER_CROP))
+
+        val imageSlider = findViewById<ImageSlider>(R.id.image_slider)
+        imageSlider.setImageList(imageList)
     }
 }
